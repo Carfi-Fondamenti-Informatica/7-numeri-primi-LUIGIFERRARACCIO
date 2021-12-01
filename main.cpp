1ee9db18ee0ef2f8685b6c6fee0c93efe9c926a0
LUIGIFERRARACCIO <91878510+LUIGIFERRARACCIO@users.noreply.github.com>
2021-12-01 17:06:52 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,5 +1,19 @@
-#include <iostream>
-
 int main(){
-  return 0;
-}
+   int numero=0;
+   bool a;
+   do{
+   cin>>numero;}while(numero<=1);
+  
+   a=primo(2,numero);
+  
+    if(a==0)
+    {
+        cout<<"numero non primo"<<endl;
+    }
+    else
+    {
+        cout<<"numero primo"<<endl;
+    }
+    return 0;
+   
+ }
